5a9f5cc1c2306fd4231455550d0e901a04527385
Tezza <terry.keenanwarland@my.jcu.edu.au>
2024-09-27 09:35:24 +1000

    Refactor the password checker to suit the practical task

--- prac_02/password_checker.py
+++ prac_02/password_checker.py
@@ -1,16 +1,17 @@
 """CP1404 Practical 2. Password Stars."""
 
 def main():
-    min_length = 8  # Set minimum length for the password
-
-    # Function to get a valid password from the user
+    min_length = 8
     password = input(f"Enter a password with {min_length} characters: ")
+    password = get_password(min_length, password)
+    print_asterisks(password)
+
+def get_password(min_length, password):
     while len(password) < min_length:
         print(f"Password must be at least {min_length} characters long.")
         password = input("Enter a password: ")
+    return password
 
-    # Print asterisks equal to the length of the password
+def print_asterisks(password):
     print("*" * len(password))
-
-
 main()
