a0e1f11356ae323c5480758a824cc6eb09a41d58
Tezza <terry.keenanwarland@my.jcu.edu.au>
2024-10-18 10:20:09 +1000

    Add the hex colours file

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,16 @@
+"""CP1404 Practical 5 - Hex Colours"""
+
+"""Based on the state name example program above, create a program that allows 
+you to look up hexadecimal colour codes like those at http://www.color-hex.com/color-names.html
+
+Use a constant dictionary of about 10 colour names and write a program that allows a user 
+to enter a name and get the code, e.g., entering AliceBlue (or aliceblue - make it case-independent) should show #f0f8ff.
+
+Entering an invalid colour name should not crash the program.
+Allow the user to enter names until they enter a blank one to stop the loop.
+
+Note
+We have just done two exercises that use dictionaries that are constants and named in ALL_CAPS.
+Please don't think this is any kind of rule or pattern.
+Dictionaries that change would not be constants."""
+
