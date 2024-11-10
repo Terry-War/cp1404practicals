0b2d5f17e9093a0c7f9ed402bb5852d440fa4d82
Tezza <terry.keenanwarland@my.jcu.edu.au>
2024-11-10 19:05:28 +1000

    Add the completed guitar file with the user input and saving feature

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,9 +1,10 @@
 """CP1404 Prac 07 - guitar
-Estimated time: 30 minutes
+Estimated time: 45 minutes
 Start at: 18:50
-Finish at:
-Total time:  minutes
+Finish at: 17:50
+Total time: 60 minutes
 Pinched alot of the code from the walkthrough example to make this work as intended, pretty happy with how it turned out.
+Also had to remember how to use open() file method to save data, haven't used it for a while.
 """
 import csv
 
@@ -58,6 +59,28 @@ def main():
     # Print the sorted list of guitars
     for guitar in guitars:
         print(guitar)
+
+    # Ask the user to input their guitars to the csv list
+    print("My New guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
+
+    # Write all guitars back to the CSV file
+    with open('guitars.csv', 'w') as file:
+        # Write header
+        file.write("Name,Year,Cost\n")
+        # Save each user's guitar data
+        for guitar in guitars:
+            file.write(f"{guitar.name},{guitar.year},{guitar.cost:.2f}\n")
+
+    print("\nAll guitars saved to 'guitars.csv'.")
+
 
 if __name__ == "__main__":
     main()
