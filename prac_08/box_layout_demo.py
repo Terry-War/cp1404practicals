7b4d44961b7cb67caac89cac83c652d031a31aeb
Tezza <terry.keenanwarland@my.jcu.edu.au>
2024-11-19 13:42:36 +1000

    Add the completed walkthrough example files

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,3 +1,10 @@
+"""CP1404 Programming 2 - prac 8 walkthrough
+Estimated: 20
+Start: 13:30
+Finish: 13:45
+Total: 15 minutes
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
 
@@ -7,6 +14,14 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        print("test")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
